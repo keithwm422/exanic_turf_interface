1e1195d727c4540f05435f11c38580bc063e5173
keithwm422 <keithwm.422@gmail.com>
2022-05-06 17:12:37 -0400

    Added member functions to turf_packet class that simulate a read or a write depending on data. Updated test code to use them and show examples.

--- turf_packet.py
+++ turf_packet.py
@@ -1,21 +1,51 @@
 # class for turf packet interface, bytes object is created at initialization, 4 bytes long only or else 4 bytes of all 0.
 # to get just a byte from a bytes object, .,...
 # eventually, need a packet class that expands on this, initializing with zero-filled bytes object of max length: bytes(MAX_LENGTH)
-ADDR_C = b'\xFF\xFF\xFF\xF0'
-TAG_C = b'\x00\x00\x00\x0F'
+ADDR_C = b'\xFF\xFF\xFF\xF0' # this is a bytes class
+ADDR_bytearray = 0xFFFFFFF0 # this is a bytes ARRAY
+TAG_C = b'\x00\x00\x00\x0F' # this is a bytes class
+TAG_bytearray=0b00001111 # this is a bytes array
+# 2 byte ascii are b'Tr' and b'Tw' respectively, these are ports (note difference to addresses in a packet which is sent to a port)
+UDP_RD = b'\x54\x72' # this is in hex, instead decimal: 21618 # obviously for reads
+UDP_WR = b'\x54\x77' # this is in hex, instead decimal: 21623 # obviously for writes
+# listen for return packets on the b'Tx' port.
+UDP_TX=b'Tx' # in hex would be: 0x5478, instead decimal: 21,624 # obviously for reads
 ENDI='little'
 class packet:
-      def __init__(self,msg):
-            #methods for msg
-            if len(msg) == 4:
-                  self.msg = msg
+      def __init__(self,hdr,data=[]): # default data to empty list, so its a read by default
+            #methods for header
+            if len(hdr) == 4: # 32 bit "hdr" 28 bit address and 4 bit tag
+                  self.hdr = hdr
             else:
-                  self.msg=b'\x00\x00\x00\x00'
-            self.address = (int.from_bytes(msg, ENDI) & 
-                            int.from_bytes(ADDR_C,ENDI)).to_bytes(max(len(msg), len(ADDR_C)), ENDI)
-            self.tag = (int.from_bytes(msg, ENDI) & 
-                        int.from_bytes(TAG_C, ENDI)).to_bytes(max(len(msg), len(TAG_C)), ENDI)
+                  self.hdr=b'\x00\x00\x00\x00'
+            if len(data) == 4: # 32-bits of data per write?
+                  self.data = data
+                  self.is_read = False
+            else: # if not 32 bits in data, then default to a read
+                  self.data = []
+                  self.is_read = True
+            self.address = (int.from_bytes(self.hdr, ENDI) & 
+                            int.from_bytes(ADDR_C,ENDI)).to_bytes(max(len(self.hdr), len(ADDR_C)), ENDI)
+            self.tag = (int.from_bytes(self.hdr, ENDI) & 
+                        int.from_bytes(TAG_C, ENDI)).to_bytes(max(len(self.hdr), len(TAG_C)), ENDI)
 
-            self.tag4bit = self.tag[-1] & 0b1111
+            self.tag4bit = self.tag[-1] & TAG_bytearray
+            self.addr28bit = ((self.address[0] << 24) | (self.address[1] << 16) | (self.address[2] << 8) | self.address[3] & ~self.tag[-1] ) & ADDR_bytearray
+            if self.is_read is False:
+                  self.send_write()
+            else:
+                  self.send_read()
       def print_all(self):
-            print('msg is : x{} \t addr is: x{} \t tag is: x{}'.format(self.msg.hex('x'),self.address.hex('x'),self.tag.hex('x')))
+            if self.is_read is False:
+                  print('hdr is : x{} \t addr is: x{} \t tag is: x{} \t data is: {}'.format(
+                        self.hdr.hex('x'),self.address.hex('x'),self.tag.hex('x'),self.data.hex('x')))
+            else:
+                  print('hdr is : x{} \t addr is: x{} \t tag is: x{} \t data is: {}'.format(
+                        self.hdr.hex('x'),self.address.hex('x'),self.tag.hex('x'),self.data))
+      def send_read(self):
+            # place holder print of what should be happening once port and all work
+            print('reading addr {}, and data should be empty: {}'.format(self.address.hex('x'), self.data))
+      def send_write(self):
+            # place holder print of what should be happening once port and all work
+            print('hdr is : x{} \t addr is: x{} \t tag is: x{}'.format(self.hdr.hex('x'),self.address.hex('x'),self.tag.hex('x')))
+            print('writing addr {}, with data: {}'.format(self.address.hex('x'), self.data.hex('x')))
