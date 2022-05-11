1a5a84df0ffc4d9307418aec807ec7c4cb25c071
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-11 12:28:13 -0400

    Update mock_turf.py

--- mock_turf.py
+++ mock_turf.py
@@ -1,16 +1,23 @@
 import socket
+import random
 
 HOST = "127.0.0.2"
 PORT = 21623
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+
+while True:
     print("UDP Socket is listening")
     conn, addr = s.accept()
     print(f"Connected by {addr}")
-    with conn: 
+    with conn:
         data = conn.recv(1024)  # 1024 bytes --> buffer size
         print("Message from client:", data)
-        response = "Connection successful"
-        conn.send(response.encode("utf-8"))
+        ack = random.randrange(0, 10)
+        # ack = 6
+        print(ack)
+        if ack < 5:
+            response = "Connection successful"
+            conn.send(response.encode("utf-8"))
