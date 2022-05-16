2683a0022ef735800a9c1ed63cd41207b9494805
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-16 13:39:00 -0400

    Update mock_turfV2.py

--- mock_turfV2.py
+++ mock_turfV2.py
@@ -6,21 +6,27 @@ PORT = 21623
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen()
-print("UDP Socket is waiting for connection")
-conn, addr = s.accept()
-print(f"Connected by {addr}")
-quit = b"q"
 while True:
-    data = conn.recv(1024)  # 1024 bytes --> buffer size
-    print(bytes(data))
-    if len(data) != 0 and quit != bytes(data):
-        print("Message from client:", data)
-        # ack = random.randrange(0, 10)
-        ack = 6
-        print(ack)
-        print(type(conn))
-        if ack < 5:
-            response = "Connection successful"
-            conn.send(response.encode("utf-8"))
-    else:
-        break
+    print("UDP Socket is waiting for connection")
+    conn, addr = s.accept()
+    print(f"Connected by {addr}")
+    quit = b'q'
+    newdata = ''
+    cont = True
+    with conn:
+        while cont is True:
+            data = conn.recv(1024)  # 1024 bytes --> buffer size
+            if newdata != data: 
+                print("Message from client: {}".format(data))
+            if len(data) != 0 and quit != data:
+                ack = random.randrange(0, 10)
+                #ack = 6
+                print(ack)
+                if ack < 5:
+                    response = "Connection successful"
+                    conn.send(response.encode("utf-8"))
+            elif len(data) != 0 and quit == data:
+                print("Connection by {} lost".format(addr))
+                print()
+                cont = False
+            newdata = data
