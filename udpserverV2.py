15e446e6e9516b889d1557ce22380b9434e66b3a
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-16 15:38:56 -0400

    Update udpserverV2.py

--- udpserverV2.py
+++ udpserverV2.py
@@ -1,17 +1,13 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 65432
+HOST = "127.0.0.2"
+PORT = 21623
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
     print("UDP Socket is listening")
-    conn, addr = s.accept()
+    data, addr = s.recvfrom(1024)
     print(f"Connected by {addr}")
-    with conn:
-        message = conn.recv(1024)  # 1024 bytes --> buffer size
-        message = message.decode("utf-8")
-        print("Message from client:", message)
-        response = "Connection successful"
-        conn.send(response.encode("utf-8"))
+    print("Message from client:", data)
+    response = "Connection successful"
+    s.sendto(response.encode("utf-8"), addr)
