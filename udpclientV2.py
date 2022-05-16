a3b7636a693d4d9eb0fc10295871f77d4e118956
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-16 15:32:34 -0400

    Update udpclientV2.py

--- udpclientV2.py
+++ udpclientV2.py
@@ -3,7 +3,7 @@ import socket
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
     message = "Hello"
     s.send(message.encode("utf-8"))
