0d18d33f50efdc543b097e1754cec8ef284de08b
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-16 16:13:45 -0400

    Add files via upload

---
+++ turf_write.py
@@ -0,0 +1,31 @@
+import socket
+import random
+
+HOST = "127.0.0.3"
+PORT = 21623
+
+while True: 
+    print("UDP Socket is waiting for connection")
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
+    cont = True
+    while cont is True:
+        data, addr = s.recvfrom(1024)
+        print(f"Connected by {addr}")
+        quit = b'q'
+        newdata = ''
+        #while cont is True:
+        if newdata != data: 
+            print("Message from client: {}".format(data))
+        if len(data) != 0 and quit != data:
+            ack = random.randrange(0, 10)
+            # ack = 4
+            if ack < 5:
+                response = "Connection successful"
+                s.sendto(response.encode("utf-8"), addr)
+        elif len(data) != 0 and quit == data:
+            print("Connection by {} lost".format(addr))
+            print()
+            cont = False
+        print(newdata)
+        newdata = data
