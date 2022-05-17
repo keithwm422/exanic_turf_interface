3db90b59e8ca295fc1bcc28060ee0dd1b61e620e
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-17 12:49:13 -0400

    Update sfc_interface_udp.py

--- sfc_interface_udp.py
+++ sfc_interface_udp.py
@@ -1,28 +1,30 @@
 # class for turf packet interface, bytes object is created at initialization, 4 bytes long only or else 4 bytes of all 0.
 # to get just a byte from a bytes object, .,...
 # eventually, need a packet class that expands on this, initializing with zero-filled bytes object of max length: bytes(MAX_LENGTH)
-from ctypes import addressof
 import socket
 
 ADDR_C = b"\x0F\xFF\xFF\xFF"  # this is a bytes class
 ADDR_bytearray = 0x0FFFFFFF  # this is a bytes ARRAY
 TAG_C = b"\xF0\x00\x00\x00"  # this is a bytes class
-TAG_bytearray = 0b11110000
-READ_bytearray = b"\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"  # this is a bytes array
+TAG_bytearray = 0b11110000  # this is a bytes class ARRAY
+
 # 2 byte ascii are b'Tr' and b'Tw' respectively, these are ports (note difference to addresses in a packet which is sent to a port)
 UDP_RD = b"\x54\x72"  # this is in hex, instead decimal: 21618 # obviously for reads
 UDP_WR = b"\x54\x77"  # this is in hex, instead decimal: 21623 # obviously for writes
+
 # listen for return packets on the b'Tx' port.
 UDP_TX = b"Tx"  # in hex would be: 0x5478, instead decimal: 21,624
 ENDI = "little"
 UDP_IP = "127.0.0.3"
-ATTEMPT = 5  #  this is the number of times to attempt a connection
-TIMEOUT = 1  #  the time to attempt a connection, written in seconds
+
+ATTEMPT = 2  #  this is the number of times to attempt a connection
+TIMEOUT = 1  #  the time to wait for a response, written in seconds
 
 MY_IP = "127.0.0.1"
 MY_PORT = 21347
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((MY_IP, MY_PORT))
+
+cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create a control socket
+cs.bind((MY_IP, MY_PORT))  # have to bind to port to make it bidirectional
 
 
 class packet:
@@ -30,12 +32,12 @@ class packet:
         self, hdr, data=[]
     ):  # default data to empty list, so its a read by default
         # methods for header
-        if len(hdr) == 4:  # 32 bit "hdr" 28 bit address and 4 bit tag
+        if len(hdr) == 4:  # 32 bit "hdr" --> 28 bit address and 4 bit tag
             self.hdr = hdr
             self.is_badPacket = False
         else:
             self.is_badPacket = True
-        if len(data) == 4:  # 32-bits of data per write?
+        if len(data) == 4:  # 32-bits of data per write
             self.data = data
             self.is_read = False
         else:  # if not 32 bits in data, then default to a read
@@ -63,64 +65,59 @@ class packet:
             else:
                 self.send_read()
 
-    def print_all(self):
-        if self.is_badPacket is True:
-            print("Bad packet format")
-        else:
-            if self.is_read is False:
-                self.print_wr()
-                self.print_ack()
-            else:
-                self.print_rd()
-                self.print_ack()
-
     def send_read(self):
-        self.sending_port = 21618
+        self.sending_port = 21618  # 'Tr' --> receives read requests
         self.recd()
 
     def send_write(self):
-        self.sending_port = 21623
+        self.sending_port = 21623  # 'Tw' --> receives write requests
         self.recd()
 
     def recd(self):
-        self.s = s
+        self.control_sock = cs
         self.attempt = ATTEMPT
-        self.s.settimeout(
+        self.control_sock.settimeout(
             TIMEOUT
         )  # sets the parameter for how long you want to listen for a response on the client side
-        # self.s.bind((MY_IP, MY_PORT)) # binds to the server
-        self.s.connect((UDP_IP, self.sending_port))  # connect to the correct port
+        self.control_sock.connect(
+            (UDP_IP, self.sending_port)
+        )  # connect to the correct port
         while True:
             if self.attempt == 0:  # if you finish an alloted amount of attempts
-                self.quit()  # comment out to not close the connection between client and server
+                # self.quit() # sends a quit message to server
                 break
             else:
                 self.conn()
                 if self.is_recv is True:
-                    self.quit()  # comment out to keep the connection between client and server open
+                    # self.quit()
                     break
                 self.attempt -= 1
 
     def conn(self):
-        self.is_recv = False
-        if len(self.data) != 0:
+        self.is_recv = False  # indicates whether a response has been received
+        if len(self.data) != 0:  # if read request
             message = self.data + self.hdr
-            self.s.sendto(message, (UDP_IP, self.sending_port))
+            self.control_sock.sendto(message, (UDP_IP, self.sending_port))
         else:
-            self.s.sendto(self.hdr, (UDP_IP, self.sending_port))
-        try:
-            self.ack, _ = self.s.recvfrom(1024)
-        except Exception:
-            self.ack = ""
-        if len(self.ack) != 0:
+            self.control_sock.sendto(self.hdr, (UDP_IP, self.sending_port))
+        try:  # attempts to receive a response
+            # if a response is received, save value
+            self.ack, _ = self.control_sock.recvfrom(
+                1024
+            )  # can format to save receiving port address
             self.is_recv = True
+        except Exception:
+            self.ack = (
+                ""  # if no response is received, indicate there wasn't a response
+            )
 
-    def quit(self):
+    def quit(self):  # most likely not required because this needs a specific
+        # server set up --> sends quit message
         message = "q"
         self.nmessage = message.encode()
-        self.s.sendto(self.nmessage, (UDP_IP, self.sending_port))
+        self.control_sock.sendto(self.nmessage, (UDP_IP, self.sending_port))
 
-    def print_ack(self):
+    def print_ack(self):  # prints what is send back from the server
         if self.is_recv is True:
             print("Received: {}".format(self.ack))
             print(
@@ -129,7 +126,7 @@ class packet:
         else:
             print("No acknowledgement after {} attempts".format(ATTEMPT))
 
-    def print_rd(self):
+    def print_rd(self):  # prints the info from read request
         print(
             "reading addr {}, and data should be empty: {}".format(
                 self.address.hex("x"), self.data
@@ -144,7 +141,7 @@ class packet:
             )
         )
 
-    def print_wr(self):
+    def print_wr(self):  # prints the info from write request
         print(
             "writing addr {}, with data: {}".format(
                 self.address.hex("x"), self.data.hex("x")
@@ -155,4 +152,16 @@ class packet:
                 self.hdr.hex("x"), self.address.hex("x"), self.tag.hex("x")
             )
         )
+
+    def print_all(self):  # prints everything in print_rd(), print_wr(), print_ack()
+        if self.is_badPacket is True:
+            print("Bad packet format")
+        else:
+            if self.is_read is False:
+                self.print_wr()
+                self.print_ack()
+            else:
+                self.print_rd()
+                self.print_ack()
+
 
