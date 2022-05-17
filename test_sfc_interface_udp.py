8925c4e9f2904c1a6cec42c2c56ea494a4b42d66
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-17 11:48:01 -0400

    Update test_sfc_interface_udp.py

--- test_sfc_interface_udp.py
+++ test_sfc_interface_udp.py
@@ -1,7 +1,6 @@
 import sfc_interface_udp
 
 
-
 msg = b"\x12\x55\xB1\xA5"  # Bits 31:28 are tag, 27:0 are address
 data = b"\x15\x26\x37\x48"
 # so far a bad message still does a read by default of addr: 0x0000000 with tag 0x0
@@ -10,17 +9,21 @@ badpacket = sfc_interface_udp.packet(
     badmsg
 )  # construct a 'packet' instance with an iniatilized byte array
 badpacket.print_all()  # print the msg and its addr and its tag
-
-print(msg.hex())
+print()
+# print(msg.hex())
 # debug code if needed to check turf packet vars
-# #print(type(tag)) #print(tag) #print(type(msg)) #print(msg.hex('x'))
+# print(type(tag)) #print(tag) #print(type(msg)) #print(msg.hex('x'))
 
 read_packet_1 = sfc_interface_udp.packet(
     msg
 )  # construct a 'packet' instance with an iniatilized byte array
 read_packet_1.print_all()  # print the hdr and its addr and its tag
-print(hex(read_packet_1.tag4bit))  # get them as byte arrays
-print(hex(read_packet_1.addr28bit))  # get them as byte arrays
+print()
+# print(hex(read_packet_1.tag4bit))  # get them as byte arrays
+# print(hex(read_packet_1.addr28bit))  # get them as byte arrays
+read_packet_1.print_rd()
+read_packet_1.print_ack()
+print()
 
 msg = b"\x12\x55\xB1\xA5"  # Bits 31:28 are tag, 27:0 are address
 data = b"\x15\x26\x37\x48"
@@ -29,5 +32,9 @@ write_packet_1 = sfc_interface_udp.packet(
     msg, data
 )  # construct a 'packet' instance with an iniatilized byte array
 write_packet_1.print_all()
-print(hex(write_packet_1.addr28bit))
-print(hex(write_packet_1.tag4bit))
+print()
+# print(hex(write_packet_1.addr28bit))
+# print(hex(write_packet_1.tag4bit))
+write_packet_1.print_wr()
+write_packet_1.print_ack()
+
