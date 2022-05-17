ce9221cf08f36a2107a0fcf8769ff88aaceac32b
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-17 14:01:47 -0400

    Mock read port for UDP socket

---
+++ turf_read_udp.py
@@ -0,0 +1,21 @@
+import socket
+import random
+
+HOST = "127.0.0.3"
+PORT = 21618
+
+while True: 
+    print("UDP Socket is waiting for connection")
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
+    while True:
+        data, addr = s.recvfrom(1024)
+        print(f"Connected by {addr}")
+        print("Message from client: {}".format(data))
+        print()
+        # ack = random.randrange(0, 10)
+        ack = 6
+        if ack < 5:
+            response = b'\x00\x00\x00\x00' + data
+            s.sendto(response, addr)
+       
